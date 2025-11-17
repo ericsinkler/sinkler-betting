a8222d0a703fc84d7057a6fb482fbea9eea7f359
ericsinkler <ericsinkler2543@gmail.com>
2025-11-17 17:16:13 -0500

    work?

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword } from "../firebase/auth";
-import { auth } from "./firebase.js";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 
 function register(){
